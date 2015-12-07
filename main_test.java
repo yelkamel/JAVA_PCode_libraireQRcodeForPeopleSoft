467fca3dfe2dc21c04f0c64ef70e3de8093bd1c7
EL KAMEL <yelkamel@gmail.com>
2015-12-07 13:58:43 +0000

    final

---
+++ main_test.java
@@ -0,0 +1,15 @@
+package test_jar;
+
+import sgqrcode.qrcode;
+
+public class main_test
+{
+
+	public static void main(String[] args)
+	{
+		qrcode.UrlToQRcode("https://www.facebook.com/", "/Users/YouCef/Desktop/testost.png", 128);
+		// TODO Auto-generated method stub
+
+	}
+
+}
